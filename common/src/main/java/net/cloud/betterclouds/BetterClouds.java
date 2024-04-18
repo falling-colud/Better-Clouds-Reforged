6dcb804effc4c1cab1b92b5ce92d943bfbfbdf2d
falling-colud <leon.raineri@gmail.com>
2024-04-18 20:02:48 +0200

    Removed Architectury requirement

--- common/src/main/java/net/cloud/betterclouds/BetterClouds.java
+++ common/src/main/java/net/cloud/betterclouds/BetterClouds.java
@@ -1,11 +1,5 @@
 package net.cloud.betterclouds;
 
-import dev.architectury.event.EventResult;
-import dev.architectury.event.events.common.EntityEvent;
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-
 public class BetterClouds {
 
     public static void init () {
